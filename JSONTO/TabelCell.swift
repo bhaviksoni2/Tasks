7e7f4d5bcf5b57ba2935310a854bb7158be2248f
bhaviksoni2 <bhavik.s@canopas.com>
2019-05-24 12:14:45 +0530

    Task1 Coompleted

---
+++ JSONTO/TabelCell.swift
@@ -0,0 +1,24 @@
+//
+//  TabelCell.swift
+//  JSONTO
+//
+//  Created by Brijesh Patel on 24/05/19.
+//  Copyright © 2019 Brijesh Patel. All rights reserved.
+//
+
+import UIKit
+
+class TabelCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
