5744348dc708defe86497e7197caad9c3e206b48
bhaviksoni2 <50912182+bhaviksoni2@users.noreply.github.com>
2019-05-29 15:35:32 +0530

    Task4 Completed Modified

--- JSONTO/ViewController.swift
+++ JSONTO/ViewController.swift
@@ -1,46 +1,29 @@
-//
-//  ViewController.swift
-//  JSONTO
-//
-//  Created by Brijesh Patel on 23/05/19.
-//  Copyright © 2019 Brijesh Patel. All rights reserved.
-//
-
 import UIKit
 import SQLite
 import Alamofire
-class Users:  NSObject, Decodable{
-  let id: Int
+class Users: Decodable{
   let name: String
-  let username: String
   let email: String
   let address: City
-  let phone: String
-  let website: String
-  init(id: Int,name: String,username: String, email: String, address: City, phone: String, website: String) {
-    self.id = id
-    self.name = name
-    self.username = username
-    self.email =  email
+  init(name: String, email: String, address: City){
     self.address = address
-    self.phone = phone
-    self.website = website
+    self.name = name
+    self.email = email
   }
 }
 class City: Decodable {
-  let street: String
-  let suite: String
   let city: String
-  let zipcode: String
-  init(street: String, suite: String, city: String, zipcode: String) {
-    self.street = street
-    self.suite = suite
+  init(city: String){
     self.city = city
-    self.zipcode = zipcode
   }
 }
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
   var user = [Users]()
+  let name = Expression<String>("name")
+  let email =  Expression<String>("emial")
+  let city = Expression<String>("city")
+  let usersTable = Table("uSer4")
+  var db : Connection!
   @IBOutlet weak var tableView: UITableView!
   lazy var refresher: UIRefreshControl = {
     let refreshControl = UIRefreshControl()
@@ -50,23 +33,62 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   }()
   override func viewDidLoad() {
     super.viewDidLoad()
-    jsonParse()
+    let fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("UsersData28.sqlite")
+    print(fileUrl.path)
+    do{
+      db = try Connection(fileUrl.path)
+      print(type(of: db))
+      createTable()
+    }
+    catch{
+      print("Error Making Connection")
+    }
     if #available(iOS 10.0, *){
       tableView.refreshControl = refresher
     }else {
       tableView.addSubview(refresher)
     }
   }
-   @objc func requestData() {
+  @objc func requestData() {
     print("refreshing data")
     let deadLine = DispatchTime.now() + .milliseconds(1000)
     DispatchQueue.main.asyncAfter(deadline: deadLine){
       self.refresher.endRefreshing()
     }
   }
+  func createTable(){
+      do{
+        print("Inside Do block")
+        try db.scalar(usersTable.exists)
+        print("Inside try block")
+        for user in try db.prepare(usersTable) {
+          print("Table is available")
+          guard let name = user[name] as? String else { return }
+          guard let email = user[email] as? String else { return }
+          guard let city = user[city] as? String else { return }
+          let city1 = City(city: city)
+          let user2 = Users(name: name, email: email, address: city1)
+          self.user.append(user2)
+        }
+        OperationQueue.main.addOperation ({
+          self.tableView.reloadData()
+        })
+        print("From create table")
+        jsonParse()
+      }catch{
+        print("Table is not available")
+        do{
+        try db.run(usersTable.create { t in
+          t.column(name)
+          t.column(email)
+          t.column(city)
+        })}catch{
+          print("Error Creating Table")
+        }
+        jsonParse()
+      }
+  }
   func jsonParse(){
-    let fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("UsersData22.sqlite")
-    print(fileUrl.path)
     print("Everything is Fine")
     do {
       Alamofire.request("http://jsonplaceholder.typicode.com/users").responseData { respone in
@@ -75,38 +97,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             let decoder = JSONDecoder()
             let userdata = try decoder.decode([Users].self, from: data)
             self.user = userdata
-            let db = try Connection(fileUrl.path)
-            print("Done")
-            let name = Expression<String>("name")
-            let email =  Expression<String>("emial")
-            let city = Expression<String>("city")
-            let usersTable = Table("USERS6")
-            do {
-              try db.scalar(usersTable.exists)
-              for user in try db.prepare(usersTable) {
-                print("name: \(user[name]), email: \(user[email]), city: \(user[city])")
-                // id: 1, name: Optional("Alice"), email: alice@mac.com
-              }
-            } catch {
-            }
-            try db.run(usersTable.create { t in
-              t.column(name, primaryKey: true)
-              t.column(email)
-              t.column(city)
-            })
+            print("Data Added to array by jsonParse")
             var iterator = self.user.makeIterator()
+            print("Iterator created")
             while let value = iterator.next(){
-              let insert = usersTable.insert(name <- value.name, email <- value.email, city <- value.address.city)
-              try db.run(insert)
+              print("data adding to the table")
+              let insert = self.usersTable.insert(self.name <- value.name, self.email <- value.email, self.city <- value.address.city)
+              do{
+                try self.db.run(insert)
+              }catch{
+                print(error.localizedDescription)
+              }
             }
             OperationQueue.main.addOperation ({
               self.tableView.reloadData()
             })
           }catch{
-            print("Table is already created")
-            OperationQueue.main.addOperation ({
-              self.tableView.reloadData()
-            })
+            print("Error Decoding JSON data")
           }
         }
       }
@@ -120,11 +127,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   }
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TabelCell
-    let users = user
-    cell.nameLabel.text = users[indexPath.row].name    
-    cell.emailLabel.text = users[indexPath.row].email
-    cell.cityLabel.text = users[indexPath.row].address.city
-    // add code to download the image from fruit.imageURL
+    let users = user[indexPath.row]
+    cell.nameLabel.text = users.name
+    cell.emailLabel.text = users.email
+    cell.cityLabel.text = users.address.city
     return cell
   }
   func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -134,4 +140,3 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
   }
 }
-
